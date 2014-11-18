d6813feb9761cd79d6f00a7d48deb865f3417769
RavenDT <ravendt@gmail.com>
2014-11-18 06:00:53 -0500

    Made some minor changes, fixed health & mana pot recipes

--- aa_craft.pl
+++ aa_craft.pl
@@ -10,12 +10,12 @@ my $rec_ctr = 0;
 my %craft =  (
 "afternoon sun"                 => { "hushed star" => 1, "medicinal powder" => 0.8, "mushroom" => 1 },
 "apprentice's typhoon cap"      => { "moonlight archeum dust" => 2, "leather" => 3, "blue salt wedge" => 3 },
-#"archeum ingot"                 => { "archeum ore" => 3 },
+"archeum ingot"                 => { "archeum ore" => 3 },
 "artificer's typhoon cap"       => { "craftsman's typhoon cap" => 1, "moonlight archeum shard" => 2, "leather" => 9, "blue salt wedge" => 7, "small seed oil" => 1 },
 "artisan's typhoon cap"         => { "artificer's typhoon cap" => 1, "moonlight archeum shard" => 3, "leather" => 12, "blue salt gloves" => 2, "small seed oil" => 1 },
 "cart bucket"                   => { "copper ingot" => 2, "leather" => 2 },
 "conqueror's typhoon cap"       => { "artisan's typhoon cap" => 1, "moonlight archeum crystal" => 2, "leather" => 15, "blue salt gloves" => 4, "viscous glossy oil" => 1 },
-#"copper ingot"                  => { "copper ore" => 3 },
+"copper ingot"                  => { "copper ore" => 3 },
 "craftsman's typhoon cap"       => { "apprentice's typhoon cap" => 1, "moonlight archeum dust" => 3, "leather" => 6, "blue salt wedge" => 5 },
 "deeply colored oil"            => { "dragon essence stabilizer" => 3, "quinoa" => 50, "cultivated ginseng" => 50, "sparkling shell dust" => 10, "coconut" => 10 },
 "delphinad typhoon cap"         => { "sealed delphinad cap" => 1 },
@@ -23,7 +23,7 @@ my %craft =  (
 "emulsified oil"                => { "crystal stabilizer" => 3, "bean" => 40, "rye" => 40, "pearl" => 20, "vanilla" => 5 },
 "epherium typhoon cap"          => { "sealed epherium cap" => 1 },
 "exquisite diaphragm"           => { "gold ingot" => 5, "amethyst" => 3, "diamond" => 2, "viscous glossy oil" => 1 },
-#"fabric"                        => { "cotton/wool" => 10 },
+"fabric"                        => { "cotton/wool" => 10 },
 "fabric pack"                   => { "fabric" => 100 },
 "farm wagon"                    => { "farm wagon design" => 1, "strong wheel" => 4, "solid shaft" => 1, "high power engine" => 1, "cart bucket" => 1, "scroll: farm cart" => 1 },
 "fine leather"                  => { "leather" => 10, "small seed oil" => 1 },
@@ -33,19 +33,20 @@ my %craft =  (
 "fish-find longliner"           => { "fish-find longliner design" => 1, "fishing license" => 1, "fish finder" => 1, "fish crane" => 1, "solid shaft" => 1, "lumber pack" => 2, "fabric pack" => 2, "iron pack" => 2 },
 "fragrant soup"                 => { "sweet soup" => 1, "ground grain" => 3, "rosemary" => 3 },
 "gilda dust"                    => { "gilda star" => 0.25 },
-#"gold ingot"                    => { "gold ore" => 3 },
+"gold ingot"                    => { "gold ore" => 3 },
 "hearty soup"                   => { "fragrant soup" => 1, "ground grain" => 3, "jujube" => 1 },
 "high power engine"             => { "flaming log" => 5, "silver ingot" => 10, "gold ingot" => 10, "archeum ingot" => 1 },
 "hushed star"                   => { "veiled flame" => 1, "medicinal powder" => 0.8, "lavender" => 2 },
 "illustrious typhoon cap"       => { "sealed illustrious cap" => 1 },
-#"iron ingot"                    => { "iron ore" => 3 },
+"iron ingot"                    => { "iron ore" => 3 },
 "iron pack"                     => { "iron ingot" => 100 },
 "lumber pack"                   => { "lumber" => 100 },
-#"lumber"                        => { "log" => 3 },
+"lumber"                        => { "log" => 3 },
 "mage's vapor"                  => { "medicinal powder" => 1.2 },
 "magnificent typhoon cap"       => { "sealed magnificent cap" => 1 },
 "meadow stream"                 => { "morning dew" => 1, "medicinal powder" => 1.2, "cornflower" => 0.5 },
 "mechanical spring"             => { "iron ingot" => 20, "copper ingot" => 5, "archeum ingot" => 3, "rough polish" => 1 },
+"midsummer meteor"              => { "afternoon sun" => 1, "medicinal powder" => 0.8, "lotus" => 0.5 },
 "misty memory"                  => { "mage's vapor" => 1, "medicinal powder" => 1.2, "iris" => 2 },
 "morning dew"                   => { "misty memory" => 1, "medicinal powder" => 1.2, "thistle" => 1 },
 "mossy pool"                    => { "time's river" => 1, "medicinal powder" => 1.2, "bay leaf" => 0.1 },
@@ -58,17 +59,17 @@ my %craft =  (
 "sealed epherium cap"           => { "magnificent typhoon cap" => 1, "moonlight archeum essence" => 3, "fine leather" => 6, "blue salt hammer" => 3, "emulsified oil" => 1, "gilda dust" => 7 },
 "sealed illustrious cap"        => { "conqueror's typhoon cap" => 1, "moonlight archeum crystal" => 3, "fine leather" => 4, "blue salt gloves" => 6, "viscous glossy oil" => 1, "gilda dust" => 2 },
 "sealed magnificent cap"        => { "illustrious typhoon cap" => 1, "moonlight archeum essence" => 2, "fine leather" => 5, "blue salt gloves" => 8, "emulsified oil" => 1, "gilda dust" => 4 },
-#"silver ingot"                  => { "silver ore" => 3 },
+"silver ingot"                  => { "silver ore" => 3 },
 "small seed oil"                => { "charcoal stabilizer" => 3, "rice" => 20, "corn" => 20 },
 "solid shaft"                   => { "thunderstruck tree" => 2, "sunridge ingot" => 1 },
-#"starshard ingot"               => { "sharshard chip" => 3 },
-#"stone brick"                   => { "raw stone" => 3 },
+"starshard ingot"               => { "sharshard chip" => 3 },
+"stone brick"                   => { "raw stone" => 3 },
 "strong wheel"                  => { "archeum log" => 2, "sturdy ingot" => 0.5 },
 "sturdy ingot"                  => { "iron ingot" => 8, "copper ingot" => 1, "silver ingot" => 1, "opaque polish" => 1 },
-"sun's glory"                   => { "midsummer meteor" => 1, "medicinal powder" => 0.8, "cultivated ginseng" => (10/3) },
+"sun's glory"                   => { "midsummer meteor" => 1, "medicinal powder" => 0.8, "cultivated ginseng" => 0.3 },
 "sunridge ingot"                => { "sturdy ingot" => 10, "mysterious garden powder" => 5, "rough polish" => 1 },
 "sweet soup"                    => { "vegetable soup" => 1, "ground grain" => 3, "pumpkin" => 5 },
-"time's river"                  => { "meadow stream" => 1, "medicinal powder" => 1.2, "aloe" => (10/3) },
+"time's river"                  => { "meadow stream" => 1, "medicinal powder" => 1.2, "aloe" => 0.3 },
 "vegetable soup"                => { "whole grain soup" => 1, "ground grain" => 3, "tomato" => 1 },
 "veiled flame"                  => { "medicinal powder" => 0.8 },
 "viscous glossy oil"            => { "rock salt stabilizer" => 3, "peanut" => 30, "wheat" => 30, "red coral" => 20 },
@@ -119,8 +120,8 @@ do {
         $qty = 1;
     }
 
-#    print "You entered: $input\n";
-#    print "Quantity: $qty\n";
+    #print "You entered: $input\n";
+    #print "Quantity: $qty\n";
 
     if (not %craft) {
         print "\%craft is undefined.\n";
@@ -129,7 +130,8 @@ do {
             my %list;
             add_to_list($input, $qty, \%list);
 
-            print "Materials required:\n\n";
+            $input =~ s/\b([\w']+)\b/\u$1/g;
+            print "\nMaterials required for: $qty"."x $input\n\n";
             
             my @keys = sort { $list{$a} <=> $list{$b} || $a cmp $b } keys(%list);
             my @vals = @list{@keys};
